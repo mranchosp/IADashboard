b3299dd09b343fc2d422daf48034ba441e47dab6
Luis Pérez <luis.perez@protonmail.com>
2023-05-27 05:53:41 -0600

    soporte para distancia y temperatura con tailwind y javascript

--- server/public/main.js
+++ server/public/main.js
@@ -1,29 +1,42 @@
 const socket = io();
 
-socket.on("connect", function () {
-  console.log("connected!");
+socket.on("connect", function() {
+  console.log("connected to Socket!");
 });
 
-socket.on("temp", function (data) {
+socket.on("temp", function(data) {
   let str = JSON.parse(data); //Then parse it
-  let temp = document.getElementById("temperatura");
-  let barra = document.getElementById("barra");
-  let div_distancia = document.getElementById("distancia");
+
+  // Temperatura
   let cel = str.temperature[0]["Celcius"];
   let far = str.temperature[0]["Fahrenheit"];
-  let distance = str["distance"];
-  temp.innerHTML = `${cel}°C`;
-  barra.attributes[2].value = cel;
-  barra.innerHTML = `${cel}%`;
-  div_distancia.innerHTML = `${distance} cm`;
-  if (distance < 10) {
-    div_distancia.style.color = "red";
-  } else {
-    div_distancia.style.color = "black";
-  };
-  if (cel <= 25) {
-    temp.style.color = "green";
-  } else {
-    temp.style.color = "red";
+
+  let spanTemp = document.getElementById("temperatura");
+  let barraTemp = document.getElementById("barra_temp");
+  spanTemp.textContent = `${cel}°C`;
+  barraTemp.style.setProperty('--value', `${cel}`);
+  if (cel < 27) {
+    spanTemp.classList.add("text-purpledracula-200");
+    barraTemp.classList.add("text-purpledracula-200");
+  } else if (cel > 27) {
+    spanTemp.classList.add("text-reddracula");
+    barraTemp.classList.add("text-reddracula");
   }
+  // Temperatura
+  let distance = str["distance"];
+  let snapDistancia = document.getElementById("distancia");
+  let barraDistancia = document.getElementById("barra_distancia");
+
+  snapDistancia.textContent = `${distance} cm`;
+  barraDistancia.style.setProperty('--value', `${distance}`);
+
+  if (distance < 10) {
+    console.log("distancia menor a 10");
+    snapDistancia.classList.add = "text-pinkdracula";
+    barraDistancia.classList.add = "text-pinkdracula";
+  } else {
+    console.log("distancia mayor a 10");
+    snapDistancia.classList.add = "text-reddracula";
+    barraDistancia.classList.add = "text-reddracula";
+  };
 });
