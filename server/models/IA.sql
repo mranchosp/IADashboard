425b46d25cc6f8b6a6a860b85010c68f5ce232c1
Luis Pérez <luis.perez@protonmail.com>
2023-06-02 13:26:16 -0600

    agregando definicion de tabla para mariadb

---
+++ server/models/IA.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `sensores` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `celcius` varchar(255) NOT NULL,
+  `distancia` varchar(255) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=103 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
